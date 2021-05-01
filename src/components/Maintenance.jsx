c42c37411087ac6b4963b2c44cc3254bf7ef5e3e
Marlene <marlene.liang@hotmail.com>
2021-05-01 16:33:42 -0400

    added barebones maintenance page

---
+++ src/components/Maintenance.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+
+const Maintenance = () => {
+    return(
+        <>
+        <div>Shop Under Maintenance</div>
+        <div>We'll be back soon!</div>
+        </>
+    )
+}
+
+export default Maintenance
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,15 @@ import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import ProductPage from '../components/ProductPage'
+import Maintenance from '../components/Maintenance'
 
 const IndexPage = ({location}) => {
   // alert(JSON.stringify(location))
   return (
     <Layout>
       <SEO title="Home" keywords={[`inspiratiq`,`shop`,`ecommerce`,`gatsby`,`art`,`prints`]} />
-      <ProductPage showCart={location.state ? location.state.showCart : false} showItems={location.state ? location.state.showItems : false} showMain={location.state ? location.state.showMain : false}></ProductPage>
-    </Layout>
+      <Maintenance/>
+      </Layout>
   )
 
 }
